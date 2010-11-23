ebbbf7c8850c108de3a741c55402bf07a4961c80
Guillaume <guillaume.soldera@gmail.com>
2010-11-23 20:04:13 +0100

    ajout de l'analyse du depMgt, mais regression

--- src/main/java/com/serli/maven/plugin/quality/AnalyzeDependenciesMojo.java
+++ src/main/java/com/serli/maven/plugin/quality/AnalyzeDependenciesMojo.java
@@ -21,17 +21,25 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import org.apache.maven.artifact.Artifact;
+import org.apache.maven.model.Dependency;
+import org.apache.maven.model.DependencyManagement;
+import org.apache.maven.model.Exclusion;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.project.MavenProject;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalysis;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzer;
 import org.apache.maven.shared.dependency.analyzer.ProjectDependencyAnalyzerException;
+import org.codehaus.plexus.util.StringUtils;
 import org.codehaus.plexus.util.xml.PrettyPrintXMLWriter;
 
 /**
@@ -91,6 +99,20 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
   private boolean ignoreNonCompile;
 
   /**
+   * Check for mismatches in your dependencyManagement section.
+   * 
+   * @parameter expression="${analyzeDepMgt}" default-value="true"
+   */
+  private boolean analyzeDepMgt;
+
+  /**
+   * Ignore Direct Dependency Overrides of dependencyManagement section.
+   * 
+   * @parameter expression="${ignoreDirect}" default-value="false"
+   */
+  private boolean ignoreDirect = false;
+
+  /**
    * Output the xml for the dependencies analyze report.
    * 
    * @parameter expression="${outputXML}" default-value="true"
@@ -98,9 +120,7 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
   private boolean outputXML;
 
   public void execute() throws MojoExecutionException {
-    // TODO regarder si les dependances surchargent le distributionManagement d'un parent
-    
-    
+
     File f = outputDirectory;
 
     if (!f.exists()) {
@@ -117,10 +137,11 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
       return;
     }
 
-    boolean warning;
+    boolean warningDep;
     try {
-      warning = checkDependencies();
-      if (warning && failOnWarning) {
+      warningDep = checkDependencies();
+
+      if (warningDep && failOnWarning) {
         throw new MojoExecutionException("Dependency problems found");
       }
     } catch (IOException e) {
@@ -140,8 +161,11 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
     }
 
     Set usedDeclared = analysis.getUsedDeclaredArtifacts();
+    getLog().info("usedDeclared " + usedDeclared.size());
     Set usedUndeclared = analysis.getUsedUndeclaredArtifacts();
+    getLog().info("usedUndeclared " + usedUndeclared.size());
     Set unusedDeclared = analysis.getUnusedDeclaredArtifacts();
+    getLog().info("unusedDeclared " + unusedDeclared.size());
 
     if (ignoreNonCompile) {
       Set filteredUnusedDeclared = new HashSet(unusedDeclared);
@@ -160,8 +184,13 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
       return false;
     }
 
+    MismatchDepMgtModel mismatchDepMgtModel = null;
+//    if (analyzeDepMgt) {
+//      mismatchDepMgtModel = checkDependencyManagement();
+//    }
+
     if (outputXML) {
-      StringBuffer dependenciesResult = writeDependenciesResult(usedDeclared, unusedDeclared, usedUndeclared);
+      StringBuffer dependenciesResult = writeDependenciesResult(usedDeclared, unusedDeclared, usedUndeclared, mismatchDepMgtModel);
       writeFile(dependenciesResult.toString());
     } else {
       if (!usedDeclared.isEmpty()) {
@@ -179,7 +208,87 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
 
     }
 
-    return !usedUndeclared.isEmpty() || !unusedDeclared.isEmpty();
+    boolean hasMismatch = (mismatchDepMgtModel != null && mismatchDepMgtModel.hasMismatched());
+    return !usedUndeclared.isEmpty() || !unusedDeclared.isEmpty() | hasMismatch;
+  }
+
+  /**
+   * Does the work of checking the DependencyManagement Section.
+   * 
+   * @return true if errors are found.
+   * @throws MojoExecutionException
+   */
+  private MismatchDepMgtModel checkDependencyManagement() throws MojoExecutionException {
+    boolean foundError = false;
+    MismatchDepMgtModel mismatchDepMgtModel = new MismatchDepMgtModel();
+    Map mismatch = null;
+    List exclusionErrors = null;
+    getLog().info("Found Resolved Dependency / DependencyManagement mismatches:");
+
+    List depMgtDependencies = null;
+
+    DependencyManagement depMgt = project.getDependencyManagement();
+    if (depMgt != null) {
+      depMgtDependencies = depMgt.getDependencies();
+    }
+
+    if (depMgtDependencies != null && !depMgtDependencies.isEmpty()) {
+      // put all the dependencies from depMgt into a map for quick lookup
+      Map depMgtMap = new HashMap();
+      Map exclusions = new HashMap();
+      Iterator iter = depMgtDependencies.iterator();
+      while (iter.hasNext()) {
+        Dependency depMgtDependency = (Dependency) iter.next();
+        depMgtMap.put(depMgtDependency.getManagementKey(), depMgtDependency);
+
+        // now put all the exclusions into a map for quick lookup
+        exclusions.putAll(addExclusions(depMgtDependency.getExclusions()));
+      }
+
+      // get dependencies for the project (including transitive)
+      Set allDependencyArtifacts = new HashSet(project.getArtifacts());
+
+      // don't warn if a dependency that is directly listed overrides
+      // depMgt. That's ok.
+      if (this.ignoreDirect) {
+        getLog().info("\tIgnoring Direct Dependencies.");
+        Set directDependencies = project.getDependencyArtifacts();
+        allDependencyArtifacts.removeAll(directDependencies);
+      }
+
+      // log exclusion errors
+      exclusionErrors = getExclusionErrors(exclusions, allDependencyArtifacts);
+      if (exclusionErrors!= null && exclusionErrors.size() > 0) {
+        foundError = true;        
+      }
+//      while (exclusionIter.hasNext()) {
+//        Artifact exclusion = (Artifact) exclusionIter.next();
+//        getLog().info(
+//            StringUtils.stripEnd(getArtifactManagementKey(exclusion), ":") + " was excluded in DepMgt, but version "
+//                + exclusion.getVersion() + " has been found in the dependency tree.");
+//      }
+
+      // find and log version mismatches
+      mismatch = getMismatch(depMgtMap, allDependencyArtifacts);
+      // Iterator mismatchIter = mismatch.keySet().iterator();
+      // while ( mismatchIter.hasNext() )
+      // {
+      // Artifact resolvedArtifact = (Artifact) mismatchIter.next();
+      // Dependency depMgtDependency = (Dependency) mismatch.get(
+      // resolvedArtifact );
+      // logMismatch( resolvedArtifact, depMgtDependency );
+      // }
+      if (!foundError) {
+        getLog().info("   None");
+      }
+    } else {
+      getLog().info("   Nothing in DepMgt.");
+    }
+
+    mismatchDepMgtModel.setMismatch(mismatch);
+    mismatchDepMgtModel.setExclusionErrors(exclusionErrors);
+
+    return mismatchDepMgtModel;
   }
 
   private void logArtifacts(Set artifacts, boolean warn) throws IOException {
@@ -199,7 +308,122 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
     }
   }
 
-  private StringBuffer writeDependenciesResult(Set usedDeclared, Set unusedDeclared, Set usedUndeclared) {
+  /**
+   * Returns a map of the exclusions using the Dependency ManagementKey as the
+   * keyset.
+   * 
+   * @param exclusionList
+   *          to be added to the map.
+   * @return a map of the exclusions using the Dependency ManagementKey as the
+   *         keyset.
+   */
+  public Map addExclusions(List exclusionList) {
+    Map exclusions = new HashMap();
+    if (exclusionList != null) {
+      Iterator exclusionIter = exclusionList.iterator();
+      while (exclusionIter.hasNext()) {
+        Exclusion exclusion = (Exclusion) exclusionIter.next();
+        exclusions.put(getExclusionKey(exclusion), exclusion);
+      }
+    }
+    return exclusions;
+  }
+
+  /**
+   * Returns a List of the artifacts that should have been excluded, but were
+   * found in the dependency tree.
+   * 
+   * @param exclusions
+   *          a map of the DependencyManagement exclusions, with the
+   *          ManagementKey as the key and Dependency as the value.
+   * @param allDependencyArtifacts
+   *          resolved artifacts to be compared.
+   * @return list of artifacts that should have been excluded.
+   */
+  public List getExclusionErrors(Map exclusions, Set allDependencyArtifacts) {
+    List list = new ArrayList();
+
+    Iterator iter = allDependencyArtifacts.iterator();
+    while (iter.hasNext()) {
+      Artifact artifact = (Artifact) iter.next();
+      if (exclusions.containsKey(getExclusionKey(artifact))) {
+        list.add(artifact);
+      }
+    }
+
+    return list;
+  }
+
+  public String getExclusionKey(Artifact artifact) {
+    return artifact.getGroupId() + ":" + artifact.getArtifactId();
+  }
+
+  public String getExclusionKey(Exclusion ex) {
+    return ex.getGroupId() + ":" + ex.getArtifactId();
+  }
+
+  /**
+   * Calculate the mismatches between the DependencyManagement and resolved
+   * artifacts
+   * 
+   * @param depMgtMap
+   *          contains the Dependency.GetManagementKey as the keyset for quick
+   *          lookup.
+   * @param allDependencyArtifacts
+   *          contains the set of all artifacts to compare.
+   * @return a map containing the resolved artifact as the key and the listed
+   *         dependency as the value.
+   */
+  public Map getMismatch(Map depMgtMap, Set allDependencyArtifacts) {
+    Map mismatchMap = new HashMap();
+
+    Iterator iter = allDependencyArtifacts.iterator();
+    while (iter.hasNext()) {
+      Artifact dependencyArtifact = (Artifact) iter.next();
+      Dependency depFromDepMgt = (Dependency) depMgtMap.get(getArtifactManagementKey(dependencyArtifact));
+      if (depFromDepMgt != null) {
+
+        // workaround for MNG-2961
+        dependencyArtifact.isSnapshot();
+
+        if (!depFromDepMgt.getVersion().equals(dependencyArtifact.getBaseVersion())) {
+          mismatchMap.put(dependencyArtifact, depFromDepMgt);
+        }
+      }
+    }
+    return mismatchMap;
+  }
+
+  /**
+   * This function displays the log to the screen showing the versions and
+   * information about the artifacts that don't match.
+   * 
+   * @param dependencyArtifact
+   *          the artifact that was resolved.
+   * @param dependencyFromDepMgt
+   *          the dependency listed in the DependencyManagement section.
+   * 
+   */
+  public void logMismatch(Artifact dependencyArtifact, Dependency dependencyFromDepMgt) {
+        getLog().info("\tDependency: " + StringUtils.stripEnd(dependencyFromDepMgt.getManagementKey(), ":"));
+    getLog().info("\t\tDepMgt  : " + dependencyFromDepMgt.getVersion());
+    getLog().info("\t\tResolved: " + dependencyArtifact.getBaseVersion());
+  }
+
+  /**
+   * This function returns a string comparable with Dependency.GetManagementKey.
+   * 
+   * @param artifact
+   *          to gen the key for
+   * @return a string in the form: groupId:ArtifactId:Type[:Classifier]
+   */
+  public String getArtifactManagementKey(Artifact artifact) {
+    return artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getType()
+        + ((artifact.getClassifier() != null) ? ":" + artifact.getClassifier() : "");
+  }
+
+  private StringBuffer writeDependenciesResult(Set usedDeclared, Set unusedDeclared, Set usedUndeclared,
+      MismatchDepMgtModel mismatchDepMgtModel) {
     StringWriter out = new StringWriter();
     PrettyPrintXMLWriter writer = new PrettyPrintXMLWriter(out);
     writer.startElement("dependencies");
@@ -212,8 +436,85 @@ public class AnalyzeDependenciesMojo extends AbstractMojo {
     writer.startElement("usedUndeclared");
     writer = writeDependencyXML(usedUndeclared, writer);
     writer.endElement();
+
+    if (mismatchDepMgtModel != null && mismatchDepMgtModel.hasMismatched()) {
+      writer = writeMismatch(mismatchDepMgtModel.getMismatch(), writer);
+      writer = writeExclusionErrors(mismatchDepMgtModel.getExclusionErrors(), writer);
+    }
+
     writer.endElement();
     return out.getBuffer();
+  }
+
+  private PrettyPrintXMLWriter writeExclusionErrors(List exclusionErrors, PrettyPrintXMLWriter writer) {
+    if (exclusionErrors != null && exclusionErrors.size() > 0) {
+      writer.startElement("exclusionErrors");
+      Iterator exclusionIter = exclusionErrors.iterator();
+      while (exclusionIter.hasNext()) {
+        Artifact exclusion = (Artifact) exclusionIter.next();
+        writer = writeExclusionError(writer, exclusion);
+        getLog().info(
+            StringUtils.stripEnd(getArtifactManagementKey(exclusion), ":") + " was excluded in DepMgt, but version "
+                + exclusion.getVersion() + " has been found in the dependency tree.");
+
+      }
+      writer.endElement();
+    }
+
+    return writer;
+  }
+
+  private PrettyPrintXMLWriter writeExclusionError(PrettyPrintXMLWriter writer, Artifact exclusion) {
+    if (exclusion != null) {
+      writer.startElement("dependency");
+      writer.startElement("groupId");
+      writer.writeText(exclusion.getGroupId());
+      writer.endElement();
+      writer.startElement("artifactId");
+      writer.writeText(exclusion.getArtifactId());
+      writer.endElement();
+      writer.startElement("versionFound");
+      writer.writeText(exclusion.getVersion());
+      writer.endElement();
+      writer.endElement();
+    }
+    return writer;
+  }
+
+  private PrettyPrintXMLWriter writeMismatch(Map mismatch, PrettyPrintXMLWriter writer) {
+    if (mismatch != null && mismatch.size() > 0) {
+      writer.startElement("overridenVersions");
+      Iterator mismatchIter = mismatch.keySet().iterator();
+      while (mismatchIter.hasNext()) {
+        Artifact resolvedArtifact = (Artifact) mismatchIter.next();
+        Dependency depMgtDependency = (Dependency) mismatch.get(resolvedArtifact);
+        writer = writeMismatch(resolvedArtifact, depMgtDependency, writer);
+      }
+      writer.endElement();
+    }
+    return writer;
+  }
+
+  private PrettyPrintXMLWriter writeMismatch(Artifact resolvedArtifact, Dependency depMgtDependency, PrettyPrintXMLWriter writer) {
+    if (resolvedArtifact == null || depMgtDependency == null) {
+      return writer;
+    }
+    logMismatch( resolvedArtifact, depMgtDependency );
+    writer.startElement("dependency");
+    writer.startElement("groupId");
+    writer.writeText(depMgtDependency.getGroupId());
+    writer.endElement();
+    writer.startElement("artifactId");
+    writer.writeText(depMgtDependency.getArtifactId());
+    writer.endElement();
+    writer.startElement("versionDepMgt");
+    writer.writeText(depMgtDependency.getVersion());
+    writer.endElement();
+    writer.startElement("versionResolved");
+    writer.writeText(resolvedArtifact.getBaseVersion());
+    writer.endElement();
+    writer.endElement();
+    return writer;
   }
 
   private PrettyPrintXMLWriter writeDependencyXML(Set artifacts, PrettyPrintXMLWriter writer) {
---
+++ src/main/java/com/serli/maven/plugin/quality/MismatchDepMgtModel.java
@@ -0,0 +1,40 @@
+package com.serli.maven.plugin.quality;
+
+import java.util.List;
+import java.util.Map;
+
+public class MismatchDepMgtModel {
+
+  private Map mismatch;
+  
+  private List exclusionErrors;
+
+  public Map getMismatch() {
+    return mismatch;
+  }
+
+  public void setMismatch(Map mismatch) {
+    this.mismatch = mismatch;
+  }
+
+  public List getExclusionErrors() {
+    return exclusionErrors;
+  }
+
+  public void setExclusionErrors(List exclusionErrors) {
+    this.exclusionErrors = exclusionErrors;
+  }
+  
+  public boolean hasMismatched() {
+    boolean result = false;
+    
+    if (mismatch != null && mismatch.size() > 0) {
+      result = true;
+    }
+    if (exclusionErrors != null && exclusionErrors.size() > 0) {
+      result = true;
+    }
+    return result;
+  }
+  
+}
