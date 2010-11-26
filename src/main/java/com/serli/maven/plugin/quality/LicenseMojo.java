518b60bc685853c3904958a9ff05651e249e0144
Guillaume Soldera <guillaume.soldera@gmail.com>
2010-11-26 09:49:39 -0800

    ajout d'instruction pour le Mojo pour les licenses

--- src/main/java/com/serli/maven/plugin/quality/LicenseMojo.java
+++ src/main/java/com/serli/maven/plugin/quality/LicenseMojo.java
@@ -14,7 +14,10 @@ import org.apache.maven.plugin.MojoFailureException;
 public class LicenseMojo extends AbstractMojo {
 
   public void execute() throws MojoExecutionException, MojoFailureException {
-    // TODO Auto-generated method stub
+    // TODO s'inspirer de http://maven-license-plugin.googlecode.com/svn/branches/maven-license-plugin-1.x.x/src/main/java/com/google/code/mojo/license/LicenseCheckMojo.java
+    // TODO extends de AbstractLicenseMojo, mais ecrire dans un fichier les 'missingHeader'
+
+    // TODO voir aussi le project-info pour avoir les licenses des dependances (determiner la compatibilite des licenses ?)
 
   }
 
